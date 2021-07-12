6a1cf45a30cd002cd0fb50ef39cca1292641550e
Andre Angel <andre.angel@redytec.local>
2021-07-12 17:13:12 -0500

    basic module

--- models/nombre_modulo.py
+++ models/nombre_modulo.py
@@ -9,15 +9,15 @@ class NombreClase(models.Model):
 
     name = fields.Char(string='Secuencia', copy=False, readonly=True, required=True,
                        default=lambda self: _('New'))
-    cadena_texto = fields.Char(string='Cadena de Texto')
-    numero_entero = fields.Integer(string='Número Entero')
+    cadena_texto = fields.Char(string='Cadena de Texto', required=True)
+    numero_entero = fields.Integer(string='Número Entero', readonly=True)
     numero_puntos_decimales = fields.Float(string='Número Decimal')
     variable_boleana = fields.Boolean(strin='Variable Boleana')
     texto_largo = fields.Text(string='Texto Largo / Notas')
     seleccion = fields.Selection([
         ('campo_1', 'Selección 1'),
         ('campo_2', 'Selección 2'),
-    ])
+    ], required=True)
     fecha = fields.Date(string='Fecha')
     fecha_y_hora = fields.Datetime(string='Fecha Y Hora')
 
