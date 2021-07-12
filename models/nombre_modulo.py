ee072061f7218ba00dae4ecfa80b586dda469488
Andre Angel <andre.angel@redytec.local>
2021-07-12 13:44:46 -0500

    basic module

--- models/nombre_modulo.py
+++ models/nombre_modulo.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# esto primero es forzoso
+
 from odoo import api, fields, models, _, tools
 
 
@@ -7,14 +7,14 @@ class NombreClase(models.Model):
     _name = "nombre.modulo"
     _description = "Nombre Modulo"
 
-    cadena_texto = fields.Char(string='Nombre Visible en Menú')
-    numero_entero = fields.Integer(string='Nombre Visible en Menú')
-    numero_puntos_decimales = fields.Float(string='Nombre Visible en Menú')
-    variable_boleana = fields.Boolean(strin='Nombre Visible en Menú')
-    texto_largo = fields.Text(string='Nombre Visible en Menú')
+    cadena_texto = fields.Char(string='Cadena de Texto')
+    numero_entero = fields.Integer(string='Número Entero')
+    numero_puntos_decimales = fields.Float(string='Número Decimal')
+    variable_boleana = fields.Boolean(strin='Variable Boleana')
+    texto_largo = fields.Text(string='Texto Largo / Notas')
     seleccion = fields.Selection([
-        ('campo_1', 'Nombre en Menú 1'),
-        ('campo_2', 'Nombre en Menú 2'),
+        ('campo_1', 'Selección 1'),
+        ('campo_2', 'Selección 2'),
     ])
-    fecha = fields.Date(string='Nombre en Menú')
-    fecha_y_hora = fields.Datetime(string='Nombre en Menú')
+    fecha = fields.Date(string='Fecha')
+    fecha_y_hora = fields.Datetime(string='Fecha Y Hora')
